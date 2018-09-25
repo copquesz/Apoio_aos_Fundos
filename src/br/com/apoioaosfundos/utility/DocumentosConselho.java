938f89cc5358510f7e1196677e32f51551631ae3
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-09-25 17:07:51 -0300

    incluindo a classe que contém os atributos dos documentos do conselho.

---
+++ src/br/com/apoioaosfundos/utility/DocumentosConselho.java
@@ -0,0 +1,57 @@
+package br.com.apoioaosfundos.utility;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "documentos_conselho")
+public class DocumentosConselho implements Serializable {
+
+	private static final long serialVersionUID = -3268759742443433996L;
+
+	// Arquivo referente ao Estatuto Social.
+	@Transient
+	private MultipartFile estatutoSocial;
+
+	// URL do Estatuto Social.
+	@Column(name = "url_estatuto_social", columnDefinition = "TEXT")
+	private String urlEstatutoSocial;
+
+	// Arquivo referente ao Cartão CNPJ.
+	@Transient
+	private MultipartFile cartaoCnpj;
+
+	// URL do Cartão CNPJ
+	@Column(name = "url_cartao_cnpj", columnDefinition = "TEXT")
+	private String urlCartaoCnpj;
+
+	// Arquivo referente a Ata de Eleição.
+	@Transient
+	private MultipartFile ataEleicao;
+
+	// URL da Ata de Eleição
+	@Column(name = "url_ata_eleicao", columnDefinition = "TEXT")
+	private String urlAtaEleicao;
+
+	// Arquivo referente ao Documento do Presidente.
+	@Transient
+	private MultipartFile documentoPresidente;
+
+	// URL do Documento do Presidente
+	@Column(name = "url_documento_presidente", columnDefinition = "TEXT")
+	private String urlDocumentoPresidente;
+
+}
