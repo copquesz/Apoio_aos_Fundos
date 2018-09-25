5090e943f1b07ae6d18425a225e9ae4f8296505b
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-09-25 13:32:31 -0300

    configurações iniciais da aplicaçao.

---
+++ src/br/com/apoioaosfundos/controller/MainController.java
@@ -0,0 +1,31 @@
+package br.com.apoioaosfundos.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Classe responsável por controlar a chamada inicial do projeto.
+ * 
+ * @author Lucas Copque
+ * @category controller
+ * @since 08/2018
+ *
+ */
+@Controller
+public class MainController {
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(HttpServletRequest request, Model model) {
+
+		// Recebe o contexto da requisição.
+		String path = request.getContextPath();
+		model.addAttribute("path", path);
+
+		return "principal/index";
+	}
+
+}
