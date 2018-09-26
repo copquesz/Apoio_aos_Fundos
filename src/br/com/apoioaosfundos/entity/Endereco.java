bf3cd60c7a6310463de8981e794b006e8ff54d63
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-09-26 15:49:24 -0300

    adicionando a classe com atributos de endereço.

---
+++ src/br/com/apoioaosfundos/entity/Endereco.java
@@ -0,0 +1,53 @@
+package br.com.apoioaosfundos.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "documentos_entidade")
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = -8064366882376795074L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "logradouro")
+	private String logradouro;
+
+	@Column(name = "numero")
+	private String numero;
+
+	@Column(name = "complemento")
+	private String complemento;
+
+	@Column(name = "bairro")
+	private String bairro;
+
+	@Column(name = "cidade")
+	private String cidade;
+
+	@Column(name = "estado")
+	private String estado;
+
+	@Column(name = "cep")
+	private String cep;
+
+}
