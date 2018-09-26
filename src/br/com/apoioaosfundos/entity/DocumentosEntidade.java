1a31aad8691dbf7edcd567f5ddeef701426315a0
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-09-26 15:17:43 -0300

    adicionando id nas classes.

--- src/br/com/apoioaosfundos/entity/DocumentosEntidade.java
+++ src/br/com/apoioaosfundos/entity/DocumentosEntidade.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -21,6 +24,11 @@ import lombok.ToString;
 public class DocumentosEntidade implements Serializable {
 
 	private static final long serialVersionUID = -3268759742443433996L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
 
 	// Arquivo referente ao Estatuto Social.
 	@Transient
