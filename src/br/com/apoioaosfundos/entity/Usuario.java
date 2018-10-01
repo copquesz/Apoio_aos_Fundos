802063424d48731737234a2694d30dbdb048109c
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-10-01 14:48:50 -0300

    adicionando atributo do tipo endereço na classe de usuário.

--- src/br/com/apoioaosfundos/entity/Usuario.java
+++ src/br/com/apoioaosfundos/entity/Usuario.java
@@ -3,13 +3,17 @@ package br.com.apoioaosfundos.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -63,6 +67,11 @@ public class Usuario implements Serializable {
 
 	@Column(name = "telefone_celular")
 	private String telefoneCelular;
+
+	// Chave estrangeira que relaciona o Endereço com a Entidade.
+	@JoinColumn(name = "endereco_id")
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Endereco endereco;
 
 	@Enumerated(EnumType.ORDINAL)
 	@Column(name = "tipo_usuario")
