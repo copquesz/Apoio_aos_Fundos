b6332781ea1068e1c71cb367cb4f92e1c9349304
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2018-10-10 17:37:56 -0300

    comentando as funcionalidades dos métodos

--- src/br/com/apoioaosfundos/dao/UsuarioDAO.java
+++ src/br/com/apoioaosfundos/dao/UsuarioDAO.java
@@ -14,23 +14,32 @@ public class UsuarioDAO {
 	@PersistenceContext
 	EntityManager em;
 
+	// Persiste um Usuário no Banco de Dados
 	public Usuario adicionar(Usuario usuario) {
 		em.persist(usuario);
 		return carregar(usuario.getEmail());
 	}
 
+	// Busca um Usuário no Banco de Dados, passando seu ID como parâmetro
 	public Usuario carregar(Long id) {
 		Query query = em.createQuery("SELECT us FROM Usuario us WHERE us.id = :id", Usuario.class);
 		query.setParameter("id", id);
 		return (Usuario) query.getSingleResult();
 	}
 
+	// Busca um Usuário no Banco de Dados, passando seu CPF como parâmetro
 	public Usuario carregar(String cpf) {
 		Query query = em.createQuery("SELECT us FROM Usuario us WHERE us.cpf = :cpf", Usuario.class);
 		query.setParameter("cpf", cpf);
 		return (Usuario) query.getSingleResult();
 	}
 
+	// Atualiza as informações do Usuário no Banco de Dados
+	public Usuario atualizar(Usuario usuario) {
+		return em.merge(usuario);
+	}
+
+	// Verifica se o CPF informado ja possui cadastro no Banco de Dados
 	public boolean isCadastrado(String cpf) {
 		Query query = em.createQuery("SELECT us FROM Usuario us WHERE us.cpf = :cpf", Usuario.class);
 		query.setParameter("cpf", cpf);
@@ -41,6 +50,7 @@ public class UsuarioDAO {
 		}
 	}
 
+	// Verifica se o CPF e SENHA informados está registrado no Banco de Dados
 	public boolean login(String cpf, String senha) {
 		Query query = em.createQuery("SELECT us FROM Usuario us WHERE us.cpf = :cpf AND us.senha = :senha",
 				Usuario.class);
